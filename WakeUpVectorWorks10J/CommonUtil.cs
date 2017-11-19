56bbd7627172b2e6a6c100e324c3afbd72644773
Clark Naciry <neo.ventilator@gmail.com>
2017-11-19 17:32:10 +0900

    2017.11.19 初版

---
+++ WakeUpVectorWorks10J/CommonUtil.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace WakeUpVectorWorks10J
+{
+    /// <summary>
+    /// 共通ユーティリティクラス
+    /// 　外部プログラムの検索、実行などを行うメソッド
+    /// </summary>
+    public class CommonUtil : Form1
+    {
+        // ネットワークアダプタ接続状況 "Up" (接続中)
+        private static String ADAPTER_STATUS_UP = "Up";
+        // ネットワークアダプタ接続状況 "Down" (切断中)
+        private static String ADAPTER_STATUS_DOWN = "Down";
+        
+        /// <summary>
+        /// (unused) インストール済みプログラム名の一覧を返すメソッド
+        /// 　※毎回起動時に確認に行くのは負荷になるため使用を中止
+        /// 　（業務端末から取得されるプログラム名一覧は恐らく少ないが、冗長なため）
+        /// 　　VectorWorks10Jがインストールされていない場合、ネットワークのON/OFFのみ行う。
+        /// </summary>
+        /// <returns>string[] ret インストール済みプログラム名の一覧の配列</returns>
+        public static string[] GetUninstallList()
+        {
+            List<string> ret = new List<string>();
+
+            string uninstall_path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+            Microsoft.Win32.RegistryKey uninstall = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(uninstall_path, false);
+            if (uninstall != null)
+            {
+                foreach (string subKey in uninstall.GetSubKeyNames())
+                {
+                    string appName = null;
+                    Microsoft.Win32.RegistryKey appkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(uninstall_path + "\\" + subKey, false);
+
+                    if (appkey.GetValue("DisplayName") != null)
+                        appName = appkey.GetValue("DisplayName").ToString();
+                    else
+                        appName = subKey;
+
+                    ret.Add(appName);
+                }
+            }
+            return ret.ToArray();
+        }
+
+        /// <summary>
+        /// (unused) ネットワークアダプタの一覧を取得し、有効な接続を全て切断するメソッド
+        /// 　切断したアダプタ名は再接続時に別途必要になるため、戻り値としてリストに入れて返す。
+        /// 　　※アダプタの切断と接続はAPIが必要で面倒なので用途を限定させた仕様に変更、本メソッドは未使用とする
+        /// 　　※nullが帰るので使わないこと。
+        /// </summary>
+        public static List<string> NetworkAdapterCloser()
+        {
+            // 戻り値のリスト
+            List<string> ret = new List<string>();
+
+            // ネットワークアダプタ一覧を取得
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            
+            // それぞれ名称(Name)と接続状況()を取得し、接続状況が"Up"の場合のみ名称を戻り値のリストに格納する
+            foreach (NetworkInterface adapter in nics)
+            {
+                // 接続中のアダプタのみ切断し、アダプタ名をリストに格納
+                if (adapter.OperationalStatus.ToString() == ADAPTER_STATUS_UP)
+                {
+                    // 可能ならここで切断APIを呼ぶクラス、メソッドをコール
+                    ret.Add(adapter.Name);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ WakeUpVectorWorks10J/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace WakeUpVectorWorks10J
+{
+    public partial class Form1 : Form
+    {
+        private String BATCH_NAME_CLOSE = "close";
+        private String BATCH_NAME_OPEN = "open";
+        private String VECTOR_PATH = "C:\\Program Files\\VectorWorks10J\\VectorWorks.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// ネットワーク切断ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CallBatch(BATCH_NAME_CLOSE);
+        }
+
+        /// <summary>
+        /// VectorWorks10J起動ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CallVectorWorks(VECTOR_PATH);
+        }
+
+        /// <summary>
+        /// ネットワーク接続ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CallBatch(BATCH_NAME_OPEN);
+        }
+
+        /// <summary>
+        /// バッチファイル起動用メソッド（開閉兼用）
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        private int CallBatch(string arg)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.Arguments = string.Format(@"/c {0}.bat", arg);
+
+            Process process = Process.Start(startInfo);
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+
+        /// <summary>
+        /// VectorWorks起動
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        private void CallVectorWorks(string arg)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = arg;
+            Process process = Process.Start(startInfo);
+            process.WaitForInputIdle();
+        }
+
+        /// <summary>
+        /// 本アプリを終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
